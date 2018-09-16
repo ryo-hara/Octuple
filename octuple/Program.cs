dc4c8cd06ef694b35636c4a8a99cb32b7c02fef0
ryo_hara_ <akstkr@gmail.com>
2018-09-17 03:03:25 +0900

    プロジェクト ファイルを追加します。

---
+++ octuple/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace octuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
