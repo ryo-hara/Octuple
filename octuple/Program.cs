6fdbc5430046dfad9b8680d57b7ce5414af6ca84
ryo_hara_ <akstkr@gmail.com>
2018-09-17 07:17:37 +0900

    add: マイクロなExcelファイル操作処理の追加
    セルに任意の文字列を挿入するところまで

--- octuple/Program.cs
+++ octuple/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClosedXML.Excel;
 
 namespace octuple
 {
@@ -10,6 +11,15 @@ namespace octuple
     {
         static void Main(string[] args)
         {
+            var workbook = new XLWorkbook("test.xlsx");
+            var worksheet = workbook.Worksheet(1);
+
+            var cell = worksheet.Cell(1, 2);
+            cell.Value = "Test";
+
+            workbook.Save();
+
+            return;
         }
     }
 }
